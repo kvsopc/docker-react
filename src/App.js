cade0f169ddb8dcbcc2e77dbbd854d39cc0e0ab5
KaayenaVaacha Softwares <kvsopc@gmail.com>
2019-02-26 07:56:36 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+	render() {
+		return (
+			<div className="App">
+				<header className="App-header">
+					<p>
+						Arunachala Siva!!!
+						<br />
+						Auom namo Bhagavathre Sree Ramanaaya!
+					</p>
+				</header>
+			</div>
+		);
+	}
+}
+
+export default App;
